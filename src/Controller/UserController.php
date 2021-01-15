e5822e30c472f96e6d300737f44e057c2abc8d7d
Ahmed <ab.perso@outlook.fr>
2021-01-15 04:33:00 +0100

    route annotation added

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,9 +4,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class UserController extends Controller
 {
