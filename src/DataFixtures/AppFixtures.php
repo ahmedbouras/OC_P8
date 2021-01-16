5878568f228a551b8dbe5ef5364e20eb131ebf5a
Ahmed <ab.perso@outlook.fr>
2021-01-16 01:32:26 +0100

    fixtures created + little fix

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,42 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Task;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+    
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = \Faker\Factory::create();
+        for ($i = 0; $i < 5; $i++) { 
+            $user = new User();
+            $password = $this->encoder->encodePassword($user, 'pass_' . $i);
+
+            $user->setUsername('user_' . $i)
+                 ->setPassword($password)
+                 ->setEmail('user_' . $i . '@mail.fr');
+
+            $manager->persist($user);
+
+            for ($j = 0; $j < 2; $j++) {
+                $task = new Task();
+                $task->setTitle($faker->word())
+                     ->setContent($faker->paragraph());
+
+                $manager->persist($task);
+            }
+        }
 
         $manager->flush();
     }
